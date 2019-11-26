6ee4230c75ae9fe683d3d7ae1873d95c6cd1dc48
Pavol Gumancik <gumancik.pavol@gmail.com>
2019-11-26 09:09:43 +0100

    Zmena v diode, u0 uz presne

--- proj2.c
+++ proj2.c
@@ -129,20 +129,20 @@ double diode(double u0, double r, double eps) {
   double fmid= eval_func(middle, u0, r); // hodnota v danom intervale
   int counter = 0;
 
-  while ((fabs(schotly(a, u0, r, b)) > eps) && (counter < 20000)) {
+  while ((fabs(a-b) > eps) && (counter < 2000)) {
     if (eval_func(a, u0, r) * fmid < 0)
       b = middle; // je zaporne, nachadza sa v danom intervale -> prepisem koniec
     else
       a = middle;
-    if (fabs(schotly(a, u0, r, b)) > eps){
+    if (fabs(a-b) > eps){
       middle = (a + b) / 2;
       fmid = eval_func(middle, u0, r);
     }
     counter++;
   }
-  if (counter >= 2000) {
+  /*if (counter >= 2000) {
   fprintf(stderr, "Breaks to prevent inf loop.\n");
-  }
+  }*/
   return middle; //up
 }
 
